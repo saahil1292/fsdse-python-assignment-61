0e6ac509f6fc9582a73ee5f21339ade7671173ca
saahil1292 <saahil1292@gmail.com>
2017-06-20 17:49:29 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,8 @@
 def solution(list_of_nums):
-    """Enter Code Here"""
+    dic1 = {'ODD': 0, 'EVEN': 0}
+    for elem in list_of_nums:
+        if (elem % 2 == 0):
+            dic1['EVEN'] += 1
+        else:
+            dic1['ODD'] += 1
+    return dic1
